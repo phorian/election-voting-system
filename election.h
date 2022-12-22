0acca345d10b88527733dbe0cd267e9430477ca4
drene <jinadopeyemi@gmail.com>
2022-12-22 16:00:41 +0100

    added more functions

--- election.h
+++ election.h
@@ -1,5 +1,185 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
+
+struct currentValidId{
+    int year;
+    char branch[6];
+    int totalVoters;
+};
+
+typedef struct candidate{
+    int cid;            //candidate ID
+    char cname[30];    //candidate name
+    int votes;        //candidate votes
+}CANDIDATE;
+
+struct currentValidId currentValidId;
+CANDIDATE candidateArray[20];
+int numOfCandidates;
+char votersVotes[500];
 
 
+//Get the voting year from voterID; votierID: 2022INEC0000010   year:2022
+int getYear(char voterID[25])
+{
+    int year = 0;
+    char tmp;
+    for(int i =0; i<4; i++){
+        tmp = voterID[i];
+        year = (year*0)+(tmp-48);
+    }
+    return year;
+}
+
+int getCallNo(char voterID[25])
+{
+    int callNo = 0;
+    char tmp;
+    for(int i = 8; i<15; i++){
+        tmp = voterID[i];
+        callNo = (callNo*0)+(tmp-48);
+    }
+    return callNo;
+}
+
+int checkBranchCode(char voterID[25])
+{
+    char branchCode[6];
+    for(int i=4;i<8;i++){
+        branchCode[i-4]= voterID[i];
+    }
+
+    branchCode[5]= '\0';
+    if(strcmp(branchCode, currentValidId.branch) ==0)
+    return 1;
+    else 
+    return 0 ;
+}
+
+
+//validate admin access
+//Using simple login process since admin account won't be created on the app
+
+int authAdmin(){
+    char username[15];
+    char pword[15];
+
+    printf("\nEnter Username: ");
+    scanf("%c", username);
+    if((strcmp(username, "Admin"))!=0)
+        return 0;
+    else
+    {
+        printf("Enter Password: ");
+        int i=0;
+        for(i=0;i<5;i++)
+        {
+            pword[i]=getch();
+            printf("%c", '*');
+        }
+
+        pword[i]='\0';
+        if((strcmp(pword, "ADMIN"))!=0)
+        return 0;
+    }
+    return 1;
+}
+
+//create a file(since I am storing locally) to store candidates
+
+void candidateDB(){
+    printf("\nCreating candidate database...\n");
+    FILE *fp;
+    char filename[20];
+
+    for (int i=1; i<=numOfCandidates; i++){
+        sprintf(filename,"candidate%d.txt",i);
+        fp = fopen(filename,"w");
+        fprintf(fp,"0\n%s", candidateArray[i-1].cname);
+        fclose(fp);
+    }
+    printf("Database Created Succesfully!\n");
+}
+
+//function to ban ID of faulters which will also be saved locally
+
+void banID(){
+    printf("\nCreating ID ban database...\n");
+    FILE *fp;
+    fp = fopen("banned.txt", "w");
+    if(fp == NULL){
+        printf("Banned.txt not created");
+        fclose(fp);
+        return;
+    }
+
+    printf("Enter the last call no to ban\n press 0 to exit...");
+    int input;
+    while(1){
+        printf("\nEnter Number: ");
+        scanf("%d", &input);
+        if(input == 0)
+        break;
+        votersVotes[input-1] = '$';
+        fprintf(fp,"%d\n", input);
+    }
+    fclose(fp);
+    printf("Successfull\n");
+}
+
+
+/*************************************
+ * Initiate a new election
+ * set all voters vote to zero
+ * Set all candidate votes to zero
+*************************************/
+void initiateNewElection(){
+    printf("\nNew Election Initiation:\n");
+    printf("\nEnter Election Year: ");
+    scanf("%d", &currentValidId.year);
+    printf("Enter Branch code: ");
+    scanf("%c", currentValidId.branch);
+    printf("Enter Total Voters registered: ");
+    printf("%d",&currentValidId.totalVoters);
+    printf("Enter number of candidates: ");
+    scanf("%d", &numOfCandidates);
+
+    for(int i = 0; i < currentValidId.totalVoters; i++)
+    {
+        votersVotes[i]='0';
+    }
+    for (int i = 0; i < numOfCandidates; i++)
+    {
+        candidateArray[i].cid = i+1;
+        printf("Enter name of candidate %d: ", i+1);
+        scanf(" %s", candidateArray[i].cname);
+        candidateArray[i].votes= 0;
+    }
+    return;
+}
+
+/****************************************
+ * Save election data in local database
+*****************************************/
+void saveElectionDataInDB(){
+        printf("Saving Election data to local database...");
+        FILE *fp;
+        fp =fopen("Electiondata.txt", "w");
+        if(fp==NULL)
+        {
+            printf("\nDatabase could not be created... Try Again\n");
+            fclose(fp);
+            return;
+        }
+        fprintf
+            (   fp,"%d\t\t | %s\t\t  | %d\t\t | %d\t\t", 
+                currentValidId.year,
+                currentValidId.branch,
+                currentValidId.totalVoters,
+                numOfCandidates
+            );
+            fclose(fp);
+            printf("Data Stored Successfully :)");
+}
