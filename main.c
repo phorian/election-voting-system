0d52ba17212a5fe9a0a079e0349a79d27822592b
drene <jinadopeyemi@gmail.com>
2023-01-01 15:59:48 +0100

    Done, Happy new year, Happy Voting

--- main.c
+++ main.c
@@ -2,24 +2,24 @@
 
 int main (){
     while(1){
-        char choice;
+        int choice;
         printf("Board");
         printf("\nNB: VOTERS SHOULD ONLY INPUT OPTION 1");
         printf("\n 1. Voter \n 2. Admin panel \n 3. Exit \n");
         printf("Enter Choice: ");
-        scanf("%c", &choice);
+        scanf("%d", &choice);
 
         switch (choice)
         {
-        case '1':
+        case 1:
             voterSec();
             break;
 
-        case '2':
+        case 2:
             adminPanel();
             break;
 
-        case '3':
+        case 3:
             return 0;
 
         default:
